287b4b6c0faa4c233512e9373ae3eaef9ef10576
hostel-hub <hostelhubofficial.pk@gmail.com>
2026-01-04 02:26:23 +0500

    Update learn-quran-with-translation.js

--- learn-quran-with-translation.js
+++ learn-quran-with-translation.js
@@ -1,10 +1,9 @@
-import React from "react";
-
-export default function Page() {
+// No 'export default' needed here because of the 'new Function' wrapper
+const DefaultComponent = () => {
   return (
-    <div>
-      <h1>Learn Quran with Translation</h1>
-      <p>This page is being loaded remotely from GitHub.</p>
+    <div style={{ padding: '20px', border: '1px solid green' }}>
+      <h2>Quran Translation Component</h2>
+      <p>This was loaded dynamically!</p>
     </div>
   );
-}
+};
