30e1dbd054ee68728837089fd16d0feae4c7b499
hostel-hub <hostelhubofficial.pk@gmail.com>
2026-01-04 02:00:12 +0500

    Create learn-quran-with-translation.js

---
+++ learn-quran-with-translation.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Learn Quran with Translation</h1>
+      <p>This page is being loaded remotely from GitHub.</p>
+    </div>
+  );
+}
